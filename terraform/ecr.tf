920a723bb040687d6fa6c868b31cf2e596314196
ishiishou <ishii1492@gmail.com>
2020-02-18 00:18:51 +0900

    ecrリポジトリを変更

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,9 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name                 = "wheather-container-app"
+resource "aws_ecr_repository" "ecr_repository_01" {
+  name                 = "weather-csv-api"
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_repository" "ecr_repository_02" {
+  name                 = "setup-db-job"
   image_tag_mutability = "MUTABLE"
 }
